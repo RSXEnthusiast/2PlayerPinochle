788481aa657dffa514d43c040a98041c33d5dcc6
Joel <joelkalich1111@gmail.com>
2021-04-21 00:43:13 -0700

    Initial creation of pinochle.

---
+++ Two Player Pinochle/app/src/main/java/game/twoplayerpinochle/game.java
@@ -0,0 +1,13 @@
+package game.twoplayerpinochle;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class game extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pinochle);
+    }
+}
