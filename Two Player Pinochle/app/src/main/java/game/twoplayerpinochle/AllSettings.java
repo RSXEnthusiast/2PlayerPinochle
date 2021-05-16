177f8059321532766c1a4f4a74d7733314cbb001
Joel <joelkalich1111@gmail.com>
2021-05-16 16:52:07 -0700

    Additions to GUI in prep for added functionality.

---
+++ Two Player Pinochle/app/src/main/java/game/twoplayerpinochle/AllSettings.java
@@ -0,0 +1,14 @@
+package game.twoplayerpinochle;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllSettings extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_settings);
+    }
+}
